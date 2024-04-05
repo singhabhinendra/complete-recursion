61e781281ca32b71c4721332b4a5a5efc3bbcd6f
singhabhinendra <singhabhinendra337@gmail.com>
2024-04-06 01:51:37 +0530

    done completely

--- src/main/java/recursionbasiccodes/PrintFirstNaturalNo.java
+++ src/main/java/recursionbasiccodes/PrintFirstNaturalNo.java
@@ -2,4 +2,17 @@ package recursionbasiccodes;
 
 public class PrintFirstNaturalNo {
 
+
+    public static void firstnatural(int number){
+        if(number==0){
+            return;
+        }
+
+        firstnatural(number-1);
+        System.out.println( number);
+    }
+    public static void main(String[] args) {
+        firstnatural(10);
+    }
+
 }
